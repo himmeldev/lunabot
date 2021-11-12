1729b8ac5f967052b53e97e29d87926a140e791e
himmeldev <himmeldev@gmail.com>
2021-11-12 16:34:16 -0300

    Changed how LoadFunctions is written for types

--- src/Util/Handlers/LoadFunctions.js
+++ src/Util/Handlers/LoadFunctions.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-
-const FunctionFiles = fs.readdirSync(`${__dirname}/../Functions`).filter((file) => file.endsWith(".js"));
-const FunctionRawObject = {};
-
-FunctionFiles.map((file) => {
-	const FuncName = file.split(".")[0];
-	FunctionRawObject[FuncName] = require(`${__dirname}/../Functions/${file}`)[FuncName];
-});
+const { reply } = require("../Functions/reply");
+const { CreateInstance } = require("../Functions/CreateInstance");
+const { FormatMS } = require("../Functions/FormatMS");
+const { HandleError } = require("../Functions/HandleError");
 
 module.exports = {
-	Functions: FunctionRawObject
+	Functions: {
+		reply,
+		CreateInstance,
+		FormatMS,
+		HandleError
+	}
 };
