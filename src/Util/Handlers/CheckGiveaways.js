7b2708413aa6cc5230303e5f76e6fb43ce84fae2
himmeldev <himmeldev@gmail.com>
2021-11-14 16:55:29 -0300

    Created SetGiveaway sub-function
    I'm just lazy and don't want to copy and paste code in the commands

--- src/Util/Handlers/CheckGiveaways.js
+++ src/Util/Handlers/CheckGiveaways.js
@@ -27,29 +27,33 @@ module.exports = {
 				return db.set("giveaways", AllGiveaways);
 			}
 
-			if (data.endsAt < ms("24h"))
-				setTimeout(async () => {
-					const { winners, participants } = await d.Util.GetWinners(data, d);
-					let { guild, message, channel } = data;
-					guild = client.guilds.cache.get(guild.id);
-					const msg = await channel?.messages?.fetch(message.id)?.catch((_) => null);
-
-					const embed = new MessageEmbed()
-						.setAuthor(`${data.prize} - ${guild.name}`)
-						.setFooter(`Ended`)
-						.setTimestamp(data.endsAt)
-						.setColor(`#${client.Internal.color("error")}`);
-
-					if (!msg) return;
-
-					if (!winners.length) return await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`This giveaway has been cancelled, I couldn't find any valid winners.\nHosted by: ${data.host}`)] }).catch((err) => null);
-
-					const MSGEdit = await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`Winners: <@${winners.join(">, <@")}>\nHosted by: ${data.host}`)] }).catch((err) => null);
-
-					const row = new MessageActionRow().addComponents([new MessageButton().setLabel(`Go to this giveaway`).setStyle(5).setURL(`https://discord.com/channels/${guild.id}/${channel.id}/${msg.id}`)]);
-
-					if (MSGEdit && winners.length) channel.send({ content: `Congrats on winning <@${winners.join(">, <@")}>!, You've won: **${data.prize}**`, embeds: [new MessageEmbed().setDescription(`${e.exclamation} \`|\` ${participants} Valid entrys`).setColor(`#${client.Internal.color(`${d.configuration.guild.theme}`)}`)], components: [row] });
-				}, data.endsAt - Date.now());
+			if (data.endsAt < ms("24h")) await SetGiveaway(data, d);
 		}
-	}
+	},
+	SetGiveaway
 };
+
+const SetGiveaway = async (data, d) =>
+	setTimeout(async () => {
+		const { client } = d;
+		const { winners, participants } = await d.Util.GetWinners(data, d);
+		let { guild, message, channel } = data;
+		guild = client.guilds.cache.get(guild.id);
+		const msg = await channel?.messages?.fetch(message.id)?.catch((_) => null);
+
+		const embed = new MessageEmbed()
+			.setAuthor(`${data.prize} - ${guild.name}`)
+			.setFooter(`Ended`)
+			.setTimestamp(data.endsAt)
+			.setColor(`#${client.Internal.color("error")}`);
+
+		if (!msg) return;
+
+		if (!winners.length) return await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`This giveaway has been cancelled, I couldn't find any valid winners.\nHosted by: ${data.host}`)] }).catch((err) => null);
+
+		const MSGEdit = await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`Winners: <@${winners.join(">, <@")}>\nHosted by: ${data.host}`)] }).catch((err) => null);
+
+		const row = new MessageActionRow().addComponents([new MessageButton().setLabel(`Go to this giveaway`).setStyle(5).setURL(`https://discord.com/channels/${guild.id}/${channel.id}/${msg.id}`)]);
+
+		if (MSGEdit && winners.length) channel.send({ content: `Congrats on winning <@${winners.join(">, <@")}>!, You've won: **${data.prize}**`, embeds: [new MessageEmbed().setDescription(`${e.exclamation} \`|\` ${participants} Valid entrys`).setColor(`#${client.Internal.color(`${d.configuration.guild.theme}`)}`)], components: [row] });
+	}, data.endsAt - Date.now());
