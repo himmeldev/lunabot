d62044e728ed4b53cb80685d87dd602b1b703e77
himmeldev <himmeldev@gmail.com>
2021-12-11 22:08:56 -0300

    Small changes and fixes to messageCreate

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -39,21 +39,21 @@ module.exports = new Event({
 
 		if (!message.content.toLowerCase().startsWith(Instance.configuration.prefix) && !message.content.toLowerCase().startsWith("luna")) return;
 
-		const cmd = args.shift().toLowerCase();
+		const cmd = args.shift().toLowerCase() || "";
 		if (!cmd.length) return;
 
 		const command = d.commands.find((Command) => Command.name === cmd || Command?.aliases?.includes(cmd));
 		if (!command) return;
 
 		try {
-			if (command.category === "developer" && !d.client.Internal.owner(Instance.user.id)) return;
+			if (command.category === "dev" && !d.client.Internal.owner(Instance.user.id)) return;
 
 			if (!command.dm && !message.guild) return d.Util.reply(Instance, { content: `This command can only be executed on servers!` });
 
 			await command.run(Instance);
 		} catch (error) {
 			await d.Util.HandleError(Instance, error);
-			Instance.channel.send({ content: `I'm sorry! An error has ocurred ${Instance.user}, I've already contacted my developer.` });
+			Instance.channel.send({ content: `I'm sorry! An error has ocurred ${Instance.user}, I've already contacted my developer.\n*Sigh* This error is just because my developer codes awfully, but don't worry, the problem will be fixed soon.` });
 		}
 	}
 });
