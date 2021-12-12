1fb764a57b8afe63b53986f9b9aacea0d87fdd96
himmeldev <himmeldev@gmail.com>
2021-12-11 22:07:43 -0300

    Fixed handle error function

--- src/Util/Functions/HandleError.js
+++ src/Util/Functions/HandleError.js
@@ -1,14 +1,11 @@
 const { MessageAttachment } = require("discord.js");
 const { inspect } = require("util");
-const pkg = require(process.cwd() + "/package.json");
 
 module.exports = {
 	HandleError: async (d, error) => {
-		const users = await Promise.all(pkg.author.split(" ").map(async (ID) => d.client.users.cache.get(ID) || (await d.client.users.fetch(ID))));
-
 		return await Promise.all(
-			users.map(async (Dev) => {
-				await Dev.send({ content: `A new error ocurred.\nChannel: ${d.channel}\nUser: ${d.user}\nResume: ${inspect.toString()}`, files: [new MessageAttachment(Buffer.from(inspect(error), "utf-8"), "error.js")] }).catch((error) => null);
+			d.client.devs.map(async (Dev) => {
+				await Dev.send({ content: `A new error ocurred.\nChannel: ${d.channel}\nUser: ${d.user}\nResume: ${error}`, files: [new MessageAttachment(Buffer.from(inspect(error), "utf-8"), "error.js")] }).catch((error) => null);
 			})
 		);
 	}
--- src/Util/Functions/HandleError.ts
+++ src/Util/Functions/HandleError.ts
@@ -1,14 +1,11 @@
-import { MessageAttachment, TextChannel, User } from "discord.js";
+import { MessageAttachment, User } from "discord.js";
 import { D } from "../TypeScript/Interfaces";
 const { inspect } = require("util");
-import * as pkg from "../../../package.json";
 
 export const HandleError = async (d: D, error: any) => {
-	const users = await Promise.all(pkg.author.split(" ").map(async (ID: string) => d.client.users.cache.get(ID) || (await d.client.users.fetch(ID))));
-
 	return await Promise.all(
-		users.map(async (Dev: User) => {
-			await Dev.send({ content: `A new error ocurred.\nChannel: ${d.channel}\nUser: ${d.user}\nResume: ${inspect.toString()}`, files: [new MessageAttachment(Buffer.from(inspect(error), "utf-8"), "error.js")] }).catch((error) => null);
+		d.client.devs.map(async (Dev: User) => {
+			await Dev.send({ content: `A new error ocurred.\nChannel: ${d.channel}\nUser: ${d.user}\nResume: ${error}`, files: [new MessageAttachment(Buffer.from(inspect(error), "utf-8"), "error.js")] }).catch((error) => null);
 		})
 	);
 };
