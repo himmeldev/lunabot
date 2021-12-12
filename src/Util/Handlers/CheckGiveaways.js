551361b361ae2d1f6b5b8fb88b07a7138b1b461b
himmeldev <himmeldev@gmail.com>
2021-12-11 22:08:26 -0300

    Fixed startup error JDSAJDJA

--- src/Util/Handlers/CheckGiveaways.js
+++ src/Util/Handlers/CheckGiveaways.js
@@ -2,6 +2,34 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 const ms = require("ms");
 const e = require(process.cwd() + "/emotes.json");
 
+const SetGiveaway = async (data, d, name) =>
+	setTimeout(async () => {
+		const { client, db } = d;
+		const { winners, participants } = await d.Util.GetWinners(data, d);
+		const gws = db.get("giveaways");
+		gws[name].winners = [winners, participants, participants === 0];
+		db.set("giveaways", gws);
+		let { guild, message, channel } = data;
+		guild = client.guilds.cache.get(guild.id);
+		const msg = await channel?.messages?.fetch(message.id)?.catch((_) => null);
+
+		const embed = new MessageEmbed()
+			.setAuthor(`${data.prize} - ${guild.name}`)
+			.setFooter(`Ended`)
+			.setTimestamp(data.endsAt)
+			.setColor(`#${client.Internal.color("error")}`);
+
+		if (!msg) return;
+
+		if (!winners.length) return await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`This giveaway has been cancelled, I couldn't find any valid winners.\nHosted by: ${data.host}`)] }).catch((err) => null);
+
+		const MSGEdit = await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`Winners: <@${winners.join(">, <@")}>\nHosted by: ${data.host}`)] }).catch((err) => null);
+
+		const row = new MessageActionRow().addComponents([new MessageButton().setLabel(`Go to this giveaway`).setStyle(5).setURL(`https://discord.com/channels/${guild.id}/${channel.id}/${msg.id}`)]);
+
+		if (MSGEdit && winners.length) channel.send({ content: `Congrats on winning <@${winners.join(">, <@")}>!, You've won: **${data.prize}**`, embeds: [new MessageEmbed().setDescription(`${e.exclamation} \`|\` ${participants} Valid entrys`).setColor(`#${client.Internal.color(`${d.configuration.guild.theme}`)}`)], components: [row] });
+	}, data.endsAt - Date.now());
+
 module.exports = {
 	CheckGiveaways: async (d) => {
 		const { db, client } = d;
@@ -32,31 +60,3 @@ module.exports = {
 	},
 	SetGiveaway
 };
-
-const SetGiveaway = async (data, d, name) =>
-	setTimeout(async () => {
-		const { client, db } = d;
-		const { winners, participants } = await d.Util.GetWinners(data, d);
-		const gws = db.get("giveaways");
-		gws[name].winners = [winners, participants, participants === 0];
-		db.set("giveaways", gws);
-		let { guild, message, channel } = data;
-		guild = client.guilds.cache.get(guild.id);
-		const msg = await channel?.messages?.fetch(message.id)?.catch((_) => null);
-
-		const embed = new MessageEmbed()
-			.setAuthor(`${data.prize} - ${guild.name}`)
-			.setFooter(`Ended`)
-			.setTimestamp(data.endsAt)
-			.setColor(`#${client.Internal.color("error")}`);
-
-		if (!msg) return;
-
-		if (!winners.length) return await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`This giveaway has been cancelled, I couldn't find any valid winners.\nHosted by: ${data.host}`)] }).catch((err) => null);
-
-		const MSGEdit = await msg.edit({ content: "**Giveaway Ended!**", embeds: [embed.setDescription(`Winners: <@${winners.join(">, <@")}>\nHosted by: ${data.host}`)] }).catch((err) => null);
-
-		const row = new MessageActionRow().addComponents([new MessageButton().setLabel(`Go to this giveaway`).setStyle(5).setURL(`https://discord.com/channels/${guild.id}/${channel.id}/${msg.id}`)]);
-
-		if (MSGEdit && winners.length) channel.send({ content: `Congrats on winning <@${winners.join(">, <@")}>!, You've won: **${data.prize}**`, embeds: [new MessageEmbed().setDescription(`${e.exclamation} \`|\` ${participants} Valid entrys`).setColor(`#${client.Internal.color(`${d.configuration.guild.theme}`)}`)], components: [row] });
-	}, data.endsAt - Date.now());
