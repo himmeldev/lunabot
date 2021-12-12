8600870ea1924c581a4391c910ce3ccb1493daff
himmeldev <himmeldev@gmail.com>
2021-12-11 22:23:47 -0300

    Small modifications to reminder class

--- src/Util/Classes/Reminder.js
+++ src/Util/Classes/Reminder.js
@@ -7,19 +7,20 @@ class Reminder {
 	channel;
 	guild;
 	token;
-    sentMessage;
+	sentMessage;
 
 	constructor(data) {
 		this.setAt = Date.now();
 		this.token = token(10);
 		this.name = `${data.user.id}_${this.token}`;
+		this.sentMessage.delivered = false;
 
 		for (const property of Object.keys(data)) {
 			this[property] = data[property];
 		}
 	}
 
-    ended() {
+	ended() {
 		return this.setAt < this.endsAt;
 	}
 }
--- src/Util/Classes/Reminder.ts
+++ src/Util/Classes/Reminder.ts
@@ -3,19 +3,20 @@ import { token } from "../Functions/token";
 
 export class Reminder {
 	name: string;
-	reason: string;
-	setAt: number;
+	readonly reason: string;
+	readonly setAt: number;
 	endsAt: number;
 	user: User;
 	channel?: TextChannel;
 	guild: Guild;
 	token: string;
-    sentMessage?: Message;
+	sentMessage?: { delivered: boolean; error: string };
 
 	constructor(data: { reason: string; endsAt: number; user: User; channel?: TextChannel; guild: Guild }) {
 		this.setAt = Date.now();
 		this.token = token(10);
 		this.name = `${data.user.id}_${this.token}`;
+		this.sentMessage.delivered = false;
 
 		for (const property of Object.keys(data)) {
 			this[property] = data[property];
