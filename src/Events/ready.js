9e8a1596adf2f2a8468a0d651cd3a320654e8b60
himmeldev <himmeldev@gmail.com>
2021-12-11 22:09:14 -0300

    Added 'd.client.devs' property for easier access

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,10 +1,13 @@
 const { Event } = require("../Util/Classes/Event");
 const { CheckGiveaways } = require("../Util/Handlers/CheckGiveaways");
+const { author } = require(`${process.cwd()}/package.json`);
 
 module.exports = new Event({
 	name: "ready",
 	run: async (d) => {
-		console.log(`The client '${d.client.user.tag}' is online.\nCounting data:\n[${d.commands.size}] Commands were loaded (${AliasesCount(d.commands)} Aliases.)\n[${d.client._eventsCount}] Events active.`);
+		const { client } = d;
+		client.devs = await Promise.all(author.split(" ").map(async (ID) => client.users.cache.get(ID) || (await client.users.fetch(ID))));
+		console.log(`The client '${client.user.tag}' is online.\nCounting data:\n[${d.commands.size}] Commands were loaded (${AliasesCount(d.commands)} Aliases.)\n[${client._eventsCount}] Events active.`);
 
 		await CheckGiveaways(d);
 	}
