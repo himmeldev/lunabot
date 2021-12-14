c7f53e43f86e42fc1ea6436f586c339af60e898b
himmeldev <himmeldev@gmail.com>
2021-12-13 22:32:40 -0300

    Made luna fetch all interactions on restart.

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -10,6 +10,7 @@ module.exports = new Event({
 		client.devs = await Promise.all(author.split(" ").map(async (ID) => client.users.cache.get(ID) || (await client.users.fetch(ID))));
 		console.log(`The client '${client.user.tag}' is online.\nCounting data:\n[${d.commands.size}] Commands were loaded (${AliasesCount(d.commands)} Aliases.)\n[${client._eventsCount}] Events active.`);
 
+		await d.client.application.commands.fetch().catch((_) => setTimeout(async () => await d.client.application.commands.fetch(), 15000));
 		await CheckGiveaways(d);
 		await CheckReminders(d);
 	}
